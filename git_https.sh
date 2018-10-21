44620df29f5919e6450169928e74c2e9d71e1f64
Nenetti <akinori1090@gmail.com>
2018-10-21 19:25:52 +0900

    0.1

--- git_https.sh
+++ git_https.sh
@@ -19,9 +19,7 @@ spawn git push origin $BRANCH
 	send \"${USERNAME}\n\"
 	expect -nocase \"Password\" {
 	    send \"${PASSWORD}\n\"
-	    expect \"wthaasd\" {
-		\"*$\";
-	    } \"*$\";
+
 	    spawn echo \"exit\"
         }
     }
